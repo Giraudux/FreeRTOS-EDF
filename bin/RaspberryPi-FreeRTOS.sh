6b328b4f45e95437fa8e31a5ac461611c2ae9567
Giraudux <giraudux@gmail.com>
2016-04-27 00:22:39 +0200

    update Makefile

--- bin/RaspberryPi-FreeRTOS.sh
+++ bin/RaspberryPi-FreeRTOS.sh
@@ -7,13 +7,16 @@ tar -xf gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
 
 rm gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
 
+mv gcc-arm-none-eabi-4_9-2015q3 gcc-arm-none-eabi
+
 cat << "EOF" > Makefile
 #
 #	Makefile for FreeRTOS demo on Raspberry Pi
 #
-PATH:=$(PATH):./gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/bin:./gcc-arm-none-eabi-4_9-2015q3/bin
+
 BASE=$(shell pwd)/
 BUILD_DIR=$(shell pwd)/build/
+PATH:=$(PATH):$(BASE)gcc-arm-none-eabi/arm-none-eabi/bin:$(BASE)gcc-arm-none-eabi/bin
 
 MODULE_NAME="RaspberryPi BSP"
 
@@ -38,11 +41,12 @@ kernel.syms: kernel.elf
 	$(Q)$(PRETTY) SYMS $(MODULE_NAME) $@
 	$(Q)$(OBJDUMP) -t kernel.elf > $@
 
-kernel.elf: LDFLAGS += -L "./gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3" -lgcc
-kernel.elf: LDFLAGS += -L "./gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/lib" -lc
+kernel.elf: LDFLAGS += -L "./gcc-arm-none-eabi/lib/gcc/arm-none-eabi/4.9.3"
+kernel.elf: LDFLAGS += -L "./gcc-arm-none-eabi/arm-none-eabi/lib"
+kernel.elf: LDFLAGS += -lgcc -lc
 kernel.elf: $(OBJECTS)
 	$(Q)$(LD) $(OBJECTS) -Map kernel.map -o $@ -T $(LINKER_SCRIPT) $(LDFLAGS)
-
 EOF
 
+make clean
 make
